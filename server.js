c22265a8863c8515d0af0041b36d64697a5a0d79
Niv <eliotley06@gmail.com>
2026-07-21 22:06:27 +0200

    deps: update apollo to v5

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const { ApolloServer } = require('@apollo/server');
-const { expressMiddleware } = require('@apollo/server/express4');
+const { expressMiddleware } = require('@as-integrations/express4');
 const express = require('express');
 const cors = require('cors');
 const http = require('http');
